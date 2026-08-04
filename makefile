a42fca9baad4ef90af77452452ab0ad9b5bec05c
Ji-johns <jijohnsjohnson178@gmail.com>
2026-08-05 01:03:27 +0530

    Initial commit: raw mode terminal setup with atexit cleanup

---
+++ makefile
@@ -0,0 +1,2 @@
+retro: retro.c
+	$(CC) retro.c -o retro -Wall -Wextra -pedantic -std=c99
---
+++ retro.c
@@ -0,0 +1,30 @@
+#include <termios.h>
+#include <unistd.h>
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+struct termios orig_termios;
+
+void disableRawMode(void) { tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios); }
+void enableRawMode(void) {
+
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO | ICANON);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+int main(void) {
+  enableRawMode();
+  char c;
+  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    if (iscntrl(c)) {
+      printf("%d\n", c);
+    } else {
+      printf("%d ('%c')\n", c, c);
+    }
+  }
+  return 0;
+}
